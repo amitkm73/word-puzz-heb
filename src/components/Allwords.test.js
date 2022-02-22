d58a47d4dcb6d9d0e68f12dbf91d3717c90545bd
amitkm <amitkimelman@gmail.com>
2022-02-22 11:46:56 +0200

    smoke test for ALL_WORDS array

---
+++ src/components/Allwords.test.js
@@ -0,0 +1,14 @@
+
+import ALL_WORDS from './Allwords';
+
+it('checks ALL_WORDS array is large', () => {
+  expect(ALL_WORDS.length > 50000);
+});
+
+it('finds hebrew word in the big array', () => {
+    expect(ALL_WORDS.includes("מחשבה"));
+});
+
+it('does not find gibrish word in the big array', () => {
+    expect(! ALL_WORDS.includes("ררררר"));
+});
